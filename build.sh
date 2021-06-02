f622e36fa80f3d7e69e9d602b4357149993a6979
Stuart Thomas <stuartthomas25@gmail.com>
2021-06-02 15:07:57 -0400

    update for M1 Mac

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+export CC=clang
+export CXX=clang++
+export QT_MAC_WANTS_LAYER=1
 rm -R build
 rm -R dist
 pyinstaller -y JParty.spec
--- cli.py
+++ cli.py
@@ -1,8 +1,11 @@
-from jparty.__main__ import main
 import sys
+import os
+from jparty.welcome_window import main
+
 
 if __name__ == '__main__':
     print("THIS MUST BE RUN FROM THE 'jparty' DIRECTORY")
+    os.environ['QT_MAC_WANTS_LAYER'] = '1'
     # f = open('/Users/Stuart/jparty_out.log', 'w')
     # sys.stdout = f
     # sys.stderr = f
