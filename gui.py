7fbcb27d4b67e2d6aee6aa116fb2aacdb46bbed9
Stuart Thomas <stuartthomas25@gmail.com>
2021-06-24 15:08:23 -0400

    split gui into loggin and nonlogging file

--- gui.py
+++ gui.py
@@ -1,13 +1,5 @@
-import sys
 from jparty.welcome_window import main
 
 
 if __name__ == '__main__':
-    try:
-        f = open('/Users/Stuart/jparty_out.log', 'w')
-        sys.stdout = f
-        sys.stderr = f
-    except:
-        pass
-
     main()
