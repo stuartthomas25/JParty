0fa950f36d66253ea67105a6a7490ecec93a824c
Stuart Nathan Thomas <stuartthomas25@gmail.com>
2022-03-06 11:07:42 -0500

    fix typo in helpmsg

--- jparty/helpmsg.py
+++ jparty/helpmsg.py
@@ -6,10 +6,9 @@ Welcome to JParty!
 - Click on the game board to display each question.
 - Press space immediately after reading the clue.
 - Adjudicate with the arrow keys.
-- Use the space bar to progress through the rest of the game.
+- Use the space bar to move through the rest of the game.
 
 Some general Jeopardy rules:
-- The person to correctly guess the previous clue has
-control of the board.
-- Contestants must anwer in the form of a question.
+- Answering correctly gives you control of the board.
+- Contestants must answer in the form of a question.
     """
